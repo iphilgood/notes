9923b015de273838e23e26efd4d683d9c56acafe
Phil Schilter <phil.schilter@gmail.com>
2016-10-05 20:12:23 +0200

    change put to post method
    forms can only submit post or get in method parameter

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -6,7 +6,7 @@ router.get('/', notes.getNotes);
 router.get('/new', notes.newNote);
 router.post('/new', notes.createNote);
 router.get("/edit/:id/", notes.getNote);
-router.put("/edit/:id/", notes.editNote);
+router.post("/edit/:id/", notes.editNote);
 router.delete("/:id/", notes.deleteNote);
 
 module.exports = router;
