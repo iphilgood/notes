0f6e89fb78acd23e731497bde341a6f0da79a68c
Phil Schilter <phil.schilter@gmail.com>
2016-10-12 17:39:07 +0200

    update gulpfile to only cssify application.scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-  gulp.src('styles/**/*.scss')
+  gulp.src('styles/application.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/styles/'));
 });
