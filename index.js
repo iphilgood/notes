41f048ef8362afa28120c99a68f96df2f4cc1310
Phil Schilter <phil.schilter@gmail.com>
2016-11-10 21:09:29 +0100

    lint index.js file

--- index.js
+++ index.js
@@ -1,87 +1,88 @@
-const express = require('express')
-const hbs = require('express-hbs')
-const app = express()
-const bodyParser = require('body-parser')
-const session = require('express-session')
-const MomentHandler = require('handlebars.moment')
-MomentHandler.registerHelpers(hbs)
+const express = require('express');
+const hbs = require('express-hbs');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MomentHandler = require('handlebars.moment');
+
+const app = express();
+MomentHandler.registerHelpers(hbs);
 
 // Use `.hbs` for extensions and find partials in `views/partials`.
 app.engine('hbs', hbs.express4({
-  partialsDir: __dirname + '/views/partials',
-  layoutsDir: __dirname + '/views/layout'
+  partialsDir: `${__dirname}/views/partials`,
+  layoutsDir: `${__dirname}/views/layout`,
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 
 app.use(session({
   secret: 'iminlovewiththecoco',
   resave: false,
-  saveUninitialized: true
-}))
+  saveUninitialized: true,
+}));
 
 // Middleware for session handling
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   // Style
-  if (!req.session.style) { req.session.style = 'light' }
-  if (req.query.style) { req.session.style = req.query.style }
+  if (!req.session.style) { req.session.style = 'light'; }
+  if (req.query.style) { req.session.style = req.query.style; }
 
   // Filter
-  if (!req.session.filter) { req.session.filter = false }
-  if (req.query.filter) { req.session.filter = req.query.filter }
+  if (!req.session.filter) { req.session.filter = false; }
+  if (req.query.filter) { req.session.filter = req.query.filter; }
 
   // Ordering
   if (req.query.orderBy) {
-    const orderBy = req.query.orderBy
+    const orderBy = req.query.orderBy;
 
-    if (req.session.orderBy === req.query.orderBy) {
-      const order = parseInt(req.session.order)
+    if (req.session.orderBy === orderBy) {
+      const order = parseInt(req.session.order);
       if (order === 1) {
-        req.session.order = -1
-        req.session.orderBy = orderBy
+        req.session.order = -1;
+        req.session.orderBy = orderBy;
       } else if (order === -1) {
-        req.session.order = undefined
-        req.session.orderBy = undefined
+        req.session.order = undefined;
+        req.session.orderBy = undefined;
       }
     } else {
-      req.session.orderBy = orderBy
-      req.session.order = 1
+      req.session.orderBy = orderBy;
+      req.session.order = 1;
     }
   }
 
-  next()
-})
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.use(require('method-override')(function(req, res) {
+app.use(bodyParser.json());
+app.use(require('method-override')((req) => {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
-    var method = req.body._method;
+    const method = req.body._method;
     delete req.body._method;
     return method;
   }
+  return undefined;
 }));
 
 app.use('/', require('./routes/notesRoutes'));
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(`${__dirname}/public`));
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening at http://localhost:3000');
 });
 
-hbs.registerHelper('if_eq', function(a, b, opts) {
-  if (a == b) {
+hbs.registerHelper('if_eq', function (a, b, opts) {
+  if (a === b) {
     return opts.fn(this);
-  } else {
-    return opts.inverse(this);
   }
+  return opts.inverse(this);
 });
 
-hbs.registerHelper('times', function(n, block) {
-  var accum = '';
-  for(var i = 0; i < n; ++i) {
+hbs.registerHelper('times', (n, block) => {
+  let accum = '';
+  for (let i = 0; i < n; ++i) {
     accum += block.fn(i);
   }
   return accum;
