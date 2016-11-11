d28588056a1ce632b1c5dab825bcfe51336ae920
Phil Schilter <phil.schilter@gmail.com>
2016-11-11 23:20:26 +0100

    lint routes

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const notes = require('../controller/notesController');
+
+const router = express.Router();
 
 router.get('/', notes.getNotes);
 router.get('/new', notes.newNote);
 router.post('/new', notes.createNote);
-router.get("/edit/:id/", notes.getNote);
-router.post("/edit/:id/", notes.editNote);
-router.delete("/:id/", notes.deleteNote);
+router.get('/edit/:id/', notes.getNote);
+router.post('/edit/:id/', notes.editNote);
+router.delete('/:id/', notes.deleteNote);
 
 module.exports = router;
